08458e3ad4b31f451ed9b35d7800a7f267cb0ce8
Rajesh123222 <Rajesh.Kumar3@synechron.com>
2024-06-03 19:15:03 +0530

    change the API token

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
-jira_api_token = 'ATATT3xFfGF0yU9GJnlCjdwzIrrrpVaNBQUL1sLWfLuwPEgMRGMTn4c8gHWPvxRgk7KOrkzPa2LfCu4hhms8xiC-bIXIWeVt-oUE_jq1vRfF5yCzhs9743RfXUGRtU1VB6Hl0LSGAYswic8RP-4sc2dW4Os_DpkoXpk0W3tSd0IKGYxPQILKw0c=958E9A92'  # Replace with your real API token
+jira_api_token = 'ATATT3xFfGF0c3t6O0iWnSWXLJ28F37JHsbUCmeKFtONxbIwmduooWVBVAAuV-il5fMKqJGKRhv26igARUiRIHJaDSlFcYDeVdovGCHTCgPz0_VKEecc-c8vuDJpjTacLNs1UcJL8ZVdcPmN_6Qozpfg-swQ5ASwKU3RpP41IjC_zDpmJE_P074=1DA8FEA6'
+    #'ATATT3xFfGF0yU9GJnlCjdwzIrrrpVaNBQUL1sLWfLuwPEgMRGMTn4c8gHWPvxRgk7KOrkzPa2LfCu4hhms8xiC-bIXIWeVt-oUE_jq1vRfF5yCzhs9743RfXUGRtU1VB6Hl0LSGAYswic8RP-4sc2dW4Os_DpkoXpk0W3tSd0IKGYxPQILKw0c=958E9A92'  # Replace with your real API token
 jira_url = 'https://blrinnovation.atlassian.net'
 # MongoDB connection parameters
 mongo_url = 'mongodb://localhost:27017/'  # Update with your MongoDB connection string
